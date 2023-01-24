dd154d973a17123cedec4cbe4fdb73a47ceccf09
JacobNeitzell <107662321+JacobNeitzell@users.noreply.github.com>
2023-01-24 12:50:55 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
@@ -9,4 +9,5 @@ export default function App() {
     </div>
   );
 }
+
 
