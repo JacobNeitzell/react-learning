21ded5ac07aa93304fef59630b956037b34e1487
JacobNeitzell <107662321+JacobNeitzell@users.noreply.github.com>
2023-01-23 17:03:37 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
